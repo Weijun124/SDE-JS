fa4304a3fc3ef0bbb4efafcbb4603d621b2302a5
Weijun124 <weijunhuang5@gmail.com>
2022-09-13 12:15:30 -0700

    version2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apiKey =
   "2GtM4rUXq5x4gU58dniAqURjtQBXsi-7ez8M9eJ9UNe7P72UHJtrknhKNKcH8vy6UHMfLbNe_Og0uPzUrdK48Zv9vIm2KR9iY035tz1dnrsuJiQyGy-a6JYVxlkfY3Yx";
 
-  const cardContainer = document.getElementById("cardContainer");
+  const cardContainer = document.getElementById("rightCon");
   const imgsrc="https://img.freepik.com/premium-vector/update-concept-application-loading-process-symbol-web-screen-vector-illustration-flat_186332-1253.jpg?";
 
 //use restrant id for find the review
@@ -32,9 +32,7 @@ function inputInfor(locationArea,categories){
     let endPoint = `https://cors-enabler-ns.herokuapp.com/bypass-cors?apiKey=${apiKey}&apiUrl=${encodeWeb}`;
 axios(endPoint)
     .then((result) => {
-        console.log(result.data.businesses)
       createCard(result.data.businesses)
-
     }).catch((err)=>console.error(err));
 }
 
