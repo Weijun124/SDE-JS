8a61ccfb98222182b6114089ba009ac08f428951
Weijun124 <weijunhuang5@gmail.com>
2022-09-14 08:19:18 -0700

    version 4 js

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ document.getElementById("form").addEventListener("submit", (e)=>{
       e.preventDefault();
       const locationArea=document.getElementById("location").value;
       const categories = document.getElementById("categories").value;
+      const price=document.getElementById("price").value;
+      const hot=document.getElementById("hot");
+      const reservation=document.getElementById("reservation");
+      const wheel=document.getElementById("wheelchair");
+      const checkbox=[hot,reservation,wheel];
+      const checkboxValue=checkbox.filter((x)=>Boolean(x.checked)).map(x=>x.value);
       clearArea()
-      inputInfor(locationArea,categories);
+      inputInfor(locationArea,categories,price,checkboxValue);
 })
 
+let date = new Date();
+let today = new Intl.DateTimeFormat("en-US").format(date);
 
+document.getElementById("footer").innerText=today;
 
 
 
