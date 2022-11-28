104b91b34aa8790d989e568c4628d6556450dabd
Weijun124 <98248068+Weijun124@users.noreply.github.com>
2022-11-27 17:38:52 -0800

    Update helpFunction.js

--- helpFunction.js
+++ helpFunction.js
@@ -1,5 +1,5 @@
 const apiKey =
-  "2GtM4rUXq5x4gU58dniAqURjtQBXsi-7ez8M9eJ9UNe7P72UHJtrknhKNKcH8vy6UHMfLbNe_Og0uPzUrdK48Zv9vIm2KR9iY035tz1dnrsuJiQyGy-a6JYVxlkfY3Yx";
+  "enter your API key";
 
 const defImg =
   "https://img.freepik.com/premium-vector/update-concept-application-loading-process-symbol-web-screen-vector-illustration-flat_186332-1253.jpg?";
